b904086ad71f721510c018546c5c0a007cf37693
Jorge Cue <jorge.cue@gmail>
2021-09-24 14:18:55 -0500

    Initial commit

---
+++ src/test/java/mx/jhcue/poc/kafkaevents/KafkaEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.jhcue.poc.kafkaevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaEventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
